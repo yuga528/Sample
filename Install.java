e3c0c142986eda31bbd17db84c9348bd4f9185fa
yuga528 <31730580+yuga528@users.noreply.github.com>
2017-09-13 13:30:00 +0530

    Create Install.java

---
+++ Install.java
@@ -0,0 +1,21 @@
+class Install{
+public static void main(String[] args){
+
+        String filePath = "C:/Users/Administrator/.jenkins/workspace/test/python-3.6.0-amd64.exe";
+        String filePath1 = "C:/Users/Administrator/.jenkins/workspace/test/python-3.6.*-amd64.exe";
+        if (filepath < filepath1)
+        try {
+             Process p = Runtime.getRuntime().exec(filePath1);
+             System.out.println("filePath1 is greater than filePath");
+             
+        }catch (Exception e) {
+        
+            e.printStackTrace();
+        }
+        else
+            Process p = Runtime.getRuntime().exec(filePath);
+            System.out.println("filePath is less than filePath1");
+    }
+	
+
+}

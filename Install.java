0ad95330e507412534435a15edd71a4b159fd79e
yuga528 <31730580+yuga528@users.noreply.github.com>
2017-09-13 14:16:00 +0530

    Update Install.java

--- Install.java
+++ Install.java
@@ -1,7 +1,7 @@
 class Install{
 public static void main(String[] args){
 
-        String a = "C:/Users/Administrator/.jenkins/workspace/test/npp.7.3.1.Installer.exe
+        String a = "C:/Users/Administrator/.jenkins/workspace/test/npp.7.3.1.Installer.exe";
         String b = "C:/Users/Administrator/.jenkins/workspace/test/npp.7.3.*.nstaller.exe";
         if (a < b) {
                Process p = Runtime.getRuntime().exec(filePath1);             

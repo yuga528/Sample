0d0e102aa5f9159487f2947d8a323cfae4430269
yuga528 <31730580+yuga528@users.noreply.github.com>
2017-09-13 14:45:46 +0530

    Update Install.java

--- Install.java
+++ Install.java
@@ -1,7 +1,7 @@
 class Install{
 public static void main(String[] args){
-	float a = 7.3.1
-	float b = 7.3.*
+	float a = 7.3.1;
+	float b = 7.3.*;
         String filepath = "C:/Users/Administrator/.jenkins/workspace/test/npp.7.3.1.Installer.exe";
         String filepath1 = "C:/Users/Administrator/.jenkins/workspace/test/npp.7.3.*.nstaller.exe";
         if (a < b) {

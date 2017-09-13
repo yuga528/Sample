0263534620160793c17e6d3d9921bf9bbcd1b656
yuga528 <31730580+yuga528@users.noreply.github.com>
2017-09-13 14:16:46 +0530

    Update Install.java

--- Install.java
+++ Install.java
@@ -4,11 +4,11 @@ public static void main(String[] args){
         String a = "C:/Users/Administrator/.jenkins/workspace/test/npp.7.3.1.Installer.exe";
         String b = "C:/Users/Administrator/.jenkins/workspace/test/npp.7.3.*.nstaller.exe";
         if (a < b) {
-               Process p = Runtime.getRuntime().exec(filePath1);             
+               Process p = Runtime.getRuntime().exec(b);             
                System.out.println("b is greater than a");
              
         }else {
-           Process p = Runtime.getRuntime().exec(filePath);             
+           Process p = Runtime.getRuntime().exec(a);             
            System.out.println("a is less than b");
     }
            

e2d44fb301349fc467d70a25fe5d3f1692948371
yuga528 <31730580+yuga528@users.noreply.github.com>
2017-09-13 14:15:11 +0530

    Update Install.java

--- Install.java
+++ Install.java
@@ -1,21 +1,17 @@
 class Install{
 public static void main(String[] args){
 
-        String filePath = "C:/Users/Administrator/.jenkins/workspace/test/python-3.6.0-amd64.exe";
-        String filePath1 = "C:/Users/Administrator/.jenkins/workspace/test/python-3.6.*-amd64.exe";
-        if (filepath < filepath1)
-        try {
-             Process p = Runtime.getRuntime().exec(filePath1);
-             System.out.println("filePath1 is greater than filePath");
+        String a = "C:/Users/Administrator/.jenkins/workspace/test/npp.7.3.1.Installer.exe
+        String b = "C:/Users/Administrator/.jenkins/workspace/test/npp.7.3.*.nstaller.exe";
+        if (a < b) {
+               Process p = Runtime.getRuntime().exec(filePath1);             
+               System.out.println("b is greater than a");
              
-        }catch (Exception e) {
-        else
-            Process p = Runtime.getRuntime().exec(filePath);
-            System.out.println("filePath is less than filePath1");
+        }else {
+           Process p = Runtime.getRuntime().exec(filePath);             
+           System.out.println("a is less than b");
     }
-            e.printStackTrace();
-        }
-        
+           
 	
-
+}
 }
